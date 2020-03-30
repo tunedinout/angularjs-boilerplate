51350d5832e7f85998d923e15c0b6f23d2329f3b
Narottam Kr Jha <narottam2911@gmail.com>
2020-03-30 15:13:41 +0530

    initial commit

---
+++ app/app.config.js
@@ -0,0 +1,15 @@
+angular.
+    module('myangularJSApp').
+    config(['$routeProvider',
+        function ($routeProvider) {
+            //add routes 
+            // $routeProvider.
+            // when('/somepath', {
+            //     template : '<some-component></some-component>'
+            // }).
+            // when('/somepath/:someId',{
+            //     template: '<some-other-component></some-other-component>'
+            // }).
+            // otherwise('/phones');
+
+        }])
---
+++ app/app.module.js
@@ -0,0 +1,4 @@
+angular.module('myangularJSApp',[
+    'core',
+    'ngRoute'
+])
